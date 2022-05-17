def0dd6284e309c46abc85dbf399834b19319863
Rafa <rafael.marcen@adidas-group.com>
2022-05-17 14:29:40 +0200

    Fix SPM support

--- Package.swift
+++ Package.swift
@@ -3,22 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "mParticle-Apple-Integration-Google-Analytics-Firebase",
+    name: "mParticle_Google_Analytics_Firebase",
     platforms: [ .iOS(.v12) ],
     products: [
         .library(
-            name: "mParticle-Apple-Integration-Google-Analytics-Firebase",
+            name: "mParticle_Google_Analytics_Firebase",
             targets: [
-                "mParticle-Apple-Integration-Google-Analytics-Firebase"
+                "mParticle_Google_Analytics_Firebase"
             ]),
     ],
     dependencies: [
-        .package(name: "mParticle-Apple-SDK", url: "https://github.com/mParticle/mparticle-apple-sdk", .upToNextMajor(from: "8.7.0")),
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMinor(from: "8.6.0")),
+        .package(
+            name: "mParticle-Apple-SDK",
+            url: "https://github.com/mParticle/mparticle-apple-sdk", .upToNextMajor(from: "8.2.0")),
+        .package(
+            name: "Firebase",
+            url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMinor(from: "8.6.0")),
     ],
     targets: [
         .target(
-            name: "mParticle-Apple-Integration-Google-Analytics-Firebase",
+            name: "mParticle_Google_Analytics_Firebase",
             dependencies: [
                 .product(name: "FirebaseAnalytics", package: "Firebase"),
                 .product(name: "mParticle-Apple-SDK", package: "mParticle-Apple-SDK")
@@ -26,6 +30,5 @@ let package = Package(
             path: "mParticle-Google-Analytics-Firebase",
             publicHeadersPath: "."
         )
-    ],
-    cxxLanguageStandard: .cxx11
+    ]
 )
